53982ebd294dbacab4282960165409bee1b1b0fd
Artx999 <54852665+Artx999@users.noreply.github.com>
2023-09-14 01:09:04 +0200

    Added idle and running animations

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,23 +5,49 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D _rigidBody;
+    private SpriteRenderer _spriteRenderer;
+    private Animator _animator;
+
+    private float _directionX;
     
     // Start is called before the first frame update
     void Start()
     {
         _rigidBody = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float directionX = Input.GetAxisRaw("Horizontal");
+        _directionX = Input.GetAxisRaw("Horizontal");
 
-        _rigidBody.velocity = new Vector2(directionX * 7f, _rigidBody.velocity.y);
+        _rigidBody.velocity = new Vector2(_directionX * 7f, _rigidBody.velocity.y);
         
         if (Input.GetButtonDown("Jump"))
         {
             _rigidBody.velocity = new Vector2(_rigidBody.velocity.x, 14f);
+        }
+        
+        UpdateAnimationState();
+    }
+
+    private void UpdateAnimationState()
+    {
+        if (_directionX > 0f)
+        {
+            _animator.SetBool("running", true);
+            _spriteRenderer.flipX = false;
+        }
+        else if (_directionX < 0f)
+        {
+            _animator.SetBool("running", true);
+            _spriteRenderer.flipX = true;
+        }
+        else
+        {
+            _animator.SetBool("running", false);
         }
     }
 }
