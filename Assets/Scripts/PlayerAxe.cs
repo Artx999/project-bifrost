b89dd83276ec64a2ac566147a6930212501b0a09
Guust207 <92439850+Guust207@users.noreply.github.com>
2023-09-29 18:39:13 +0200

    Setup test project

---
+++ Assets/Scripts/PlayerAxe.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAxe : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If the left mouse button is clicked.
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Get the mouse position on the screen and send a raycast into the game world from that position.
+            Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
+
+            //If something was hit, the RaycastHit2D.collider will not be null.
+            if (hit.collider != null && hit.collider.CompareTag("Player"))
+            {
+                //Debug.Log(hit.collider.name);
+            }
+        }
+    }
+}
