767e4bc2bf8607aca121cde6b33215aaca374699
Artx999 <54852665+Artx999@users.noreply.github.com>
2023-11-17 05:48:37 +0100

    Made the rope prefab more "standalone"

--- Assets/RopeHingeJoint.cs
+++ Assets/RopeHingeJoint.cs
@@ -3,16 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UIElements;
 
 public class RopeHingeJoint : MonoBehaviour
 {
     public int ropeLength;
     public float segmentLength;
+    public float ropeMass;
 
     private Transform _transform;
     
-    public GameObject anchor;
+    public GameObject anchorPrefab;
     private Rigidbody2D _anchorRigidbody;
     public GameObject ropeSegmentPrefab;
     
@@ -22,15 +24,27 @@ public class RopeHingeJoint : MonoBehaviour
         _transform = this.transform;
         
         // Generate rope
-        GameObject lastSegment = anchor;
+        GameObject lastSegment = Instantiate(anchorPrefab, _transform);;
         for (int i = 0; i < ropeLength; i++)
         {
             GameObject currentSegment = Instantiate(ropeSegmentPrefab, _transform);
-            currentSegment.GetComponent<HingeJoint2D>().connectedBody = lastSegment.GetComponent<Rigidbody2D>();
+            currentSegment.transform.localScale = new Vector3(segmentLength, segmentLength, segmentLength);
+            currentSegment.GetComponent<Rigidbody2D>().mass = ropeMass;
+
+            Rigidbody2D lastSegmentRigidbody2D = lastSegment.GetComponent<Rigidbody2D>();
+            
+            HingeJoint2D hingeJoint2D = currentSegment.GetComponent<HingeJoint2D>();
+            DistanceJoint2D distanceJoint2D = currentSegment.GetComponent<DistanceJoint2D>();
+            hingeJoint2D.connectedBody = lastSegmentRigidbody2D;
+            distanceJoint2D.connectedBody = lastSegmentRigidbody2D;
             // First segment needs to be attached in the center of the anchor
             // All other segments need to be connected further down from the last
             if (i != 0)
-                currentSegment.GetComponent<HingeJoint2D>().connectedAnchor = new Vector2(0, -1);
+            {
+                hingeJoint2D.connectedAnchor = new Vector2(0, -1);
+                distanceJoint2D.connectedAnchor = new Vector2(0, -1);
+            }
+
             lastSegment = currentSegment;
         }
     }
