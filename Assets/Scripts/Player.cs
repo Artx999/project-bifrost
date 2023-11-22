ad597fa0312a49badef116f07491bc8067be5946
Guust207 <92439850+Guust207@users.noreply.github.com>
2023-11-22 13:20:11 +0100

    Added axe aiming to the states

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private enum PlayerState
+    public enum PlayerState
     {
         Grounded,
         Fall,
@@ -18,7 +18,7 @@ public class Player : MonoBehaviour
         WallAim
     }
 
-    private PlayerState _currentState;
+    public PlayerState _currentState;
     
     // We can reference like this, because of Singleton
     // Singleton = One class can have only one instance
@@ -66,6 +66,7 @@ public class Player : MonoBehaviour
                 break;
             
             case PlayerState.GroundedAim:
+                OnGroundAim();
                 break;
             
             case PlayerState.AxeThrow:
@@ -109,45 +110,6 @@ public class Player : MonoBehaviour
 
         this._boxCollider.enabled = true;
         this._rigidbody.gravityScale = 1f;
-        
-        
-        // If the left button is pressed start the throw mechanic here
-        if (IsAiming() && IsGrounded() && !_mouseHeldDown)
-        {
-            _mouseHeldDown = true;
-            
-            // Stop the players movement completely while aiming
-            _rigidbody.velocity = Vector2.zero;
-        }
-
-        // While the player is holding down the mouse button (ie. aiming), we show a simple sight in form
-        // of a white dot
-        else if (Input.GetMouseButton(0) && _mouseHeldDown)
-        {
-            // Gets the player position, mouse position and calculates the throw vector with these two points
-            // This new vector is sent to the show sight method
-            Vector2 playerPos = transform.position;
-            Vector2 currentMousePos = GetMousePosition();
-            Vector2 currentThrowVec = GetThrowVector(playerPos, currentMousePos);
-            
-            ShowSight(currentThrowVec);
-        }
-        
-        // If the code has been through the above two blocks (ie. left button has been pressed and held down)
-        // and been released we start the actual axe throw
-        else if(_mouseHeldDown)
-        {
-            _mouseHeldDown = false;
-            _gameManager.axeIsSeperated = true;
-            sight.SetActive(false);
-
-            Vector2 playerPosition = transform.position;
-            Vector2 newMousePosition = GetMousePosition();
-            Vector2 throwVec = GetThrowVector(playerPosition, newMousePosition);
-            
-            // Reference the axe and apply the speed based on the aim vector
-            _axeThrow.ApplyAxeSpeed(throwVec);
-        }
     }
 
     private void FixedUpdate()
@@ -325,12 +287,51 @@ public class Player : MonoBehaviour
     private void OnGroundAim()
     {
         // Track mouse and show sight
+        // If the left button is pressed start the throw mechanic here
+        if (IsAiming() && IsGrounded() && !_mouseHeldDown)
+        {
+            _mouseHeldDown = true;
+            
+            // Stop the players movement completely while aiming
+            _rigidbody.velocity = Vector2.zero;
+        }
+
+        // While the player is holding down the mouse button (ie. aiming), we show a simple sight in form
+        // of a white dot
+        else if (Input.GetMouseButton(0) && _mouseHeldDown)
+        {
+            // Gets the player position, mouse position and calculates the throw vector with these two points
+            // This new vector is sent to the show sight method
+            Vector2 playerPos = transform.position;
+            Vector2 currentMousePos = GetMousePosition();
+            Vector2 currentThrowVec = GetThrowVector(playerPos, currentMousePos);
+            
+            ShowSight(currentThrowVec);
+        }
+        
+        // If the code has been through the above two blocks (ie. left button has been pressed and held down)
+        // and been released we start the actual axe throw
+        else if(_mouseHeldDown)
+        {
+            this._currentState = PlayerState.AxeThrow;
+            
+            _mouseHeldDown = false;
+            _gameManager.axeIsSeperated = true;
+            sight.SetActive(false);
+
+            Vector2 playerPosition = transform.position;
+            Vector2 newMousePosition = GetMousePosition();
+            Vector2 throwVec = GetThrowVector(playerPosition, newMousePosition);
+            
+            // Reference the axe and apply the speed based on the aim vector
+            _axeThrow.ApplyAxeSpeed(throwVec);
+        }
         
         // Throw
-        this._currentState = PlayerState.AxeThrow;
+        //this._currentState = PlayerState.AxeThrow;
         
         // Cancel
-        this._currentState = PlayerState.Grounded;
+        //this._currentState = PlayerState.Grounded;
     }
 
     private void OnAxeThrow()
