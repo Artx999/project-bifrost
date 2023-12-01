9ede99a784cd0104eebe4917ed70ae6eba046205
Guust207 <92439850+Guust207@users.noreply.github.com>
2023-12-01 13:29:47 +0100

    Finished rough main menu

--- Assets/Scripts/MenuButton.cs
+++ Assets/Scripts/MenuButton.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuButton : MonoBehaviour
 {
+    public GameObject startMenu = null;
+    public GameObject controlsMenu = null;
+    
     private Camera _camera;
     private Animator _animator;
     private PolygonCollider2D _collider;
     
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         this._camera = Camera.main;
         this._animator = GetComponent<Animator>();
@@ -19,18 +23,64 @@ public class MenuButton : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        var test = OnMouseHover();
-        this._animator.SetBool("mouseHover", test);
+        var mouseHover = OnMouseHover();
+        this._animator.SetBool("mouseHover", mouseHover);
+        
+        if (mouseHover && Input.GetMouseButtonDown(0))
+        {
+            var objectName = this.name;
+            
+            switch (objectName)
+            {
+                case "Play":
+                    Debug.Log("Play");
+                    SceneManager.LoadScene(1);
+                    break;
+                
+                case "HowToPlay":
+                    Debug.Log("How to play");
+                    this.LoadMenu(false);
+                    break;
+                
+                case "Exit":
+                    Debug.Log("Exit");
+                    Application.Quit();
+                    break;
+                
+                case "Back":
+                    Debug.Log("Back");
+                    this.LoadMenu(true);
+                    break;
+                
+                default:
+                    Debug.Log("ERROR: Check gameobject names");
+                    break;
+            }
+        }
     }
 
     private bool OnMouseHover()
     {
-        Vector2 mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+        var mousePosition = this.GetMousePosition();
 
         var hit = Physics2D.Raycast(mousePosition, Vector2.zero);
 
         return hit.collider == this._collider;
+    }
+
+    private Vector2 GetMousePosition()
+    {
+        if(this._camera != null)
+            return this._camera.ScreenToWorldPoint(Input.mousePosition);
+        
+        return Vector2.zero;
+    }
+
+    private void LoadMenu(bool isToStartMenu)
+    {
+        this.startMenu.SetActive(isToStartMenu);
+        this.controlsMenu.SetActive(!isToStartMenu);
     }
 }
