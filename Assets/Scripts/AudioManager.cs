7296cc0c9bece24e8ffe5d884d0a23dff3f87841
Guust207 <92439850+Guust207@users.noreply.github.com>
2023-12-02 13:19:43 +0100

    Added sound effects to start menu

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,15 +1,26 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
     public AudioSource sfxSource;
 
-    [Header("SFXs")]
+    [Header("Game SFXs")]
     public AudioClip axeThrow;
     public AudioClip axeHit;
     public AudioClip landing;
     public AudioClip backLanding;
     public AudioClip wallLanding;
+
+    [Header("Menu SFXs")]
+    public AudioClip buttonHover;
+    public AudioClip buttonClick;
+
+    private void Awake()
+    {
+        //sfxSource = this.AddComponent<AudioSource>();
+    }
 
     public void PlaySfx(AudioClip clip)
     {
