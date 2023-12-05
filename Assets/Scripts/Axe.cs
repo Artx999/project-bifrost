681a6eb1f3221a545479d4f2f8be56c27eb41a85
Artx999 <54852665+Artx999@users.noreply.github.com>
2023-12-05 23:32:38 +0100

    Fixed issue that stopped game from building

--- Assets/Scripts/Axe.cs
+++ Assets/Scripts/Axe.cs
@@ -1,9 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Xml.Schema;
-using TMPro.EditorUtilities;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Axe : MonoBehaviour
